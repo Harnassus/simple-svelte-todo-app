8f853cf7b296c56d698ed32dbf821c541e6b4d65
Michael Festus <micfestus45@gmail.com>
2022-07-11 14:14:09 +0100

    A simple todo app

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+  import Todos from './components/Todos.svelte';
+import Todo from './components/Todos.svelte'
+</script>
+
+<main>
+ <Todo />
+</main>
+
+<style>
+  :root {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+
+</style>
